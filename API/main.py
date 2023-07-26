2c2293812b8a1e862e0234548fd2d6bf6d8c8620
graua-git <graua@oregonstate.edu>
2023-07-25 18:57:18 -0700

    Database query with API get function

--- API/main.py
+++ API/main.py
@@ -9,14 +9,27 @@ connection = mysql.connector.connect(
     host='localhost',
     user='root',
     password='password',
-    database='tripplannerdb'
+    database='triplannerdb'
 )
+cursor = connection.cursor()
 
 class HelloWorld(Resource):
     def get(self, name, test):
         return {"data": "Hello World!", "name": name, "test": test}
     
+class Users(Resource):
+    def get(self):
+        cursor.execute("SELECT first_name, last_name FROM Users")
+        return cursor.fetchall()
+
+class Trips(Resource):
+    def get(self):
+        cursor.execute("SELECT name FROM Trips")
+        return cursor.fetchall()
+    
 api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
+api.add_resource(Users, "/users")
+api.add_resource(Trips, "/trips")
 
 if __name__ == "__main__":
     app.run(debug=True)
