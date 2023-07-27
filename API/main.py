7a3ece8c0ddae159ef6daa3781430e167b4cedf2
graua-git <graua@oregonstate.edu>
2023-07-27 14:07:35 -0700

    removed resources, added routes for users get and post

--- API/main.py
+++ API/main.py
@@ -1,30 +1,43 @@
-from flask import Flask
-from flask_restful import Api, Resource
-
-import Resources.Users as Users
-import Resources.Trips as Trips
-import Resources.Memberships as Memberships
-import Resources.Tasks as Tasks
-import Resources.Expenses as Expenses
-import Resources.Events as Events
+from flask import Flask, request, jsonify
+import mysql.connector
 
 app = Flask(__name__)
-api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self, name, test):
-        return {"data": "Hello World!", "name": name, "test": test}
+db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='password',
+    database='triplannerdb'
+)
+cursor = db.cursor()
 
-# Create Queries
-api.add_resource(Users.CreateUsers, "/users/<string:email>/<string:password>/<string:first_name>/<string:last_name>")
+# ------------------------------------ END POINTS ------------------------------------
 
-# Read Queries
-api.add_resource(Users.ReadUsers, "/users")
-api.add_resource(Trips.ReadTrips, "/trips")
-api.add_resource(Memberships.ReadMemberships, "/memberships")
-api.add_resource(Tasks.ReadTasks, "/tasks")
-api.add_resource(Expenses.ReadExpenses, "/expenses")
-api.add_resource(Events.ReadEvents, "/events")
+# ------------------ Users ------------------
+@app.route('/users', methods=['GET'])
+def read_users():
+    sql = "SELECT email, first_name, last_name FROM Users"
+    cursor.execute(sql)
+    return cursor.fetchall()
 
-if __name__ == "__main__":
+@app.route('/users', methods=['POST'])
+def create_users():
+    user = request.get_json()
+    email = user.get('email')
+    password = user.get('password')
+    first_name = user.get('first_name')
+    last_name = user.get('last_name')
+    sql = "INSERT INTO Users (email, password, first_name, last_name) VALUES (%s, %s, %s, %s)"
+    val = (email, password, first_name, last_name)
+    cursor.execute(sql, val)
+    db.commit()
+    return jsonify({'message': "Record updated successfully"})
+
+# ------------------ Trips ------------------
+# --------------- Memberships ---------------
+# ------------------ Tasks ------------------
+# ---------------- Expenses -----------------
+# ----------------- Events ------------------
+
+if __name__ == '__main__':
     app.run(debug=True)
