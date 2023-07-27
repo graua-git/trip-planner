e9e58458a3588145dd269432c3a65bb82823d6ec
graua-git <graua@oregonstate.edu>
2023-07-27 14:08:08 -0700

    structured file to test create and read queries

--- API/test.py
+++ API/test.py
@@ -1,6 +1,31 @@
 import requests
 
-BASE = "http://localhost:5000/"
+BASE = 'http://localhost:5000/'
 
-response = requests.get(BASE + "memberships")
+user = {
+    'first_name': 'Alex',
+    'last_name': 'Grau',
+    'email': 'alexgrau2@email.com',
+    'password': 'UsablePassword'
+}
+
+# ----------- CREATE -----------
+
+response = requests.post(BASE + "users", json=user)
+if response.status_code == 200:
+    print(response.status_code)
+    print("Record created successfully!")
+    print(response.json()) 
+else:
+    print(response.status_code)
+    print("Failed to create record!")
+    print(response.text)
+
+# ------------ READ ------------
+response = requests.get(BASE + "users")
+
+# ----------- UPDATE -----------
+
+# ----------- DELETE -----------
+
 print(response.json())
