ff4c26342e0fb894e74df0a940ef8e2e39ba33db
graua-git <graua@oregonstate.edu>
2023-07-29 15:30:27 -0700

    adjusted for dictionary response

--- app/src/components/TripsRow.js
+++ app/src/components/TripsRow.js
@@ -1,10 +1,10 @@
 export default function TripRow({trip, removeTrip}) {
     return (
         <tr>
-            <td>{trip[0]}</td>
-            <td>{trip[1]}</td>
-            <td>{trip[2]}</td>
-            <td>{trip[3]}</td>
+            <td>{trip.name}</td>
+            <td>{trip.start_date}</td>
+            <td>{trip.end_date}</td>
+            <td>{trip.organizer}</td>
         </tr>
     )
 }
