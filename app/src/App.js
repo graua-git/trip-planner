55704af0baa191422ca30c7e9fa877605764753d
graua-git <graua@oregonstate.edu>
2023-08-02 20:11:57 -0700

    Created login page

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import MainPage from "./pages/MainPage"
+import MainPage from "./pages/MainPage";
+import LoginPage from "./pages/LoginPage"
 import HomePage from "./pages/HomePage";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<MainPage />} />
           <Route exact path="/home" element={<HomePage />} />
+          <Route exact path="/login" element={<LoginPage />} />
         </Routes>
       </div>
     </Router>
---
+++ app/src/pages/LoginPage.js
@@ -0,0 +1,15 @@
+export default function Homepage() {
+    return (
+        <form>
+            <h3>Login</h3>
+            <label for="email">Email: </label>
+            <br />
+            <input type="text" name="email" required />
+            <br />
+            <label for="password">Password: </label>
+            <br />
+            <input type="text" name="email" required />
+            <br />
+        </form>
+    )
+}
