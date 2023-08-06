1847e8738c1c0e170e5b6dae6804d938b37b1366
graua-git <graua@oregonstate.edu>
2023-08-05 21:36:38 -0700

    Created create account form

--- app/src/pages/CreateAccountPage.js
+++ app/src/pages/CreateAccountPage.js
@@ -2,9 +2,37 @@ import url from '../api.json'
 import { useState } from 'react';
 
 export default function CreateAccountPage() {
+    const [accountData, setAccountData] = useState({
+        email: "",
+        password: "",
+        first_name: "",
+        last_name: ""
+    });
+
+    const handleFormChange = (event) => {
+        const { name, value} = event.target;
+        setAccountData((prevAccountData) => ({
+            ...prevAccountData,
+            [name]: value
+        }));
+    }
+
+    const handleCreateAccount = (event) => {
+        console.log(accountData);
+    }
+
     return (
-        <form>
-            
+        <form onSubmit={handleCreateAccount}>
+            <h3>Create Account</h3>
+            <input type="text" name="first_name" placeholder="First Name" value={accountData.first_name} onChange={handleFormChange} required />
+            <br />
+            <input type="text" name="last_name" placeholder="Last Name" value={accountData.last_name} onChange={handleFormChange} required />
+            <br />
+            <input type="text" name="email" placeholder="Email" value={accountData.email} onChange={handleFormChange} required />
+            <br />
+            <input type="text" name="password" placeholder="Password" value={accountData.password} onChange={handleFormChange} required />
+            <br />
+            <button type="submit">Create Account</button>
         </form>
     )
 }
