975dc2b32f92176723b01e437c37c125b59d2437
graua-git <graua@oregonstate.edu>
2023-08-16 15:52:49 -0700

    renamed routes

--- API/Endpoints/tasks.py
+++ API/Endpoints/tasks.py
@@ -5,16 +5,16 @@ from Endpoints.token import validate_token
 
 tasks_bp = Blueprint('tasks', __name__)
 
-@tasks_bp.route('/tasks', methods=['GET'])
+@tasks_bp.route('/read-all', methods=['GET'])
 def read_tasks():
     sql = "SELECT * FROM Tasks"
     headers = ['task_id', 'name', 'trip', 'assignee', 'created_by', 'date_created', 'time_created', 'due_date', 'due_time']
     return read(sql, headers)
 
-@tasks_bp.route('/tasks', methods=['POST'])
+@tasks_bp.route('/create', methods=['POST'])
 def create_task():
     return create(request.get_json(), "Tasks")
 
-@tasks_bp.route('/tasks/<int:task_id>', methods=['PUT'])
+@tasks_bp.route('/update/<int:task_id>', methods=['PUT'])
 def update_task(task_id):
     return update(request.get_json(), "Tasks", task_id)
