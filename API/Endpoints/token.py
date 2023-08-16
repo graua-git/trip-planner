29159d9c7f6d3c92287d6bbcdf2db24f4e73dc53
graua-git <graua@oregonstate.edu>
2023-08-16 11:34:02 -0700

    reorganizing directories

---
+++ API/Endpoints/token.py
@@ -0,0 +1,59 @@
+from flask import jsonify
+import jwt
+import json
+from datetime import datetime, timedelta
+
+location = 'API/Endpoints/QueryFunctions/key.json'
+
+def write_key():
+    """
+    writes new key for API
+    """
+    key = {
+        'key': ''
+    }
+    with open(location, 'w') as json_file:
+        json.dump(key, json_file, indent=4)
+
+def read_key():
+    """
+    returns key for API
+    """
+    with open(location, 'r') as json_file:
+        data = json.load(json_file)
+    return data['key']
+
+
+def validate_token(request):
+    """
+    Validates token from HTTP request
+    request: HTTP request
+    returns: int, user_id IF token is valid ELSE dict, message
+    """
+    token = request.headers.get('Authorization', 'NONE')
+    
+    if not token or not token.startswith('Bearer '):
+        return jsonify({'message': 'Token is missing'}), 401
+    token = token.replace('Bearer ', '')
+
+    try:
+        payload = jwt.decode(token, read_key(), algorithms=['HS256'])
+        return payload.get('user_id')
+    except jwt.ExpiredSignatureError:
+        return jsonify({'message': 'Token has expired'}), 401
+    except jwt.DecodeError:
+        return jsonify({'message': 'Invalid token'}), 401
+    
+def generate_token(user_id) -> dict:
+    """
+    Generates token for client
+    user_id: user_id in database
+    returns: dict, token
+    """
+    expires = datetime.utcnow() + timedelta(hours=1)
+    payload = {
+        'user_id': user_id,
+        'expires': expires.isoformat()
+    }
+    token = jwt.encode(payload, read_key(), algorithm='HS256')
+    return jsonify({'token': token})
--- API/Endpoints/users.py
+++ API/Endpoints/users.py
@@ -1,3 +1,52 @@
+from flask import Blueprint, request, jsonify
+from datetime import datetime, timedelta
 
-from Endpoints.QueryFunctions.crud import create, read, update, delete
-from Endpoints.QueryFunctions.validate import validate
+from Endpoints.crud import create, read, update, delete
+from Endpoints.token import validate_token, generate_token
+
+user_bp = Blueprint('users', __name__)
+
+@user_bp.route('/read-all', methods=['GET'])
+def read_users():
+    sql = "SELECT user_id, email, first_name, last_name FROM Users"
+    headers = ['user_id', 'email', 'first_name', 'last_name']
+    return read(sql, headers)
+
+@user_bp.route('/read-one', methods=['GET'])
+def get_user():
+    # Validate token
+    user_id = validate_token(request)
+    if not isinstance(user_id, int):
+        return user_id
+    
+    # Query database
+    sql = f"SELECT user_id, email, first_name, last_name FROM Users WHERE user_id = {user_id}"
+    headers = ['user_id', 'email', 'first_name', 'last_name']
+    return read(sql, headers, 'one')
+
+@user_bp.route('/login', methods=['POST'])
+def login():
+    # Validate User
+    user = request.get_json()
+    sql = f"SELECT user_id FROM Users WHERE email = '{user['email']}' AND password = '{user['password']}'"
+    headers = ['user_id']
+    try:
+        user_id = read(sql, headers, 'one')['user_id']
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+    # Generate Token
+    token = generate_token(user_id)
+    return jsonify({'token': token})
+
+@user_bp.route('/create-account', methods=['POST'])
+def create_user():
+    return create(request.get_json(), "Users")
+    
+@user_bp.route('/update-user', methods=['PUT'])
+def update_user(user_id):
+    return update(request.get_json(), "Users", user_id)
+
+@user_bp.route('/delete-user', methods=['DELETE'])
+def delete_user(user_id):
+    return delete(f"DELETE FROM Users WHERE user_id = {user_id}")
