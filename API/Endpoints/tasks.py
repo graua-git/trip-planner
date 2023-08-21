8b200db805f18d92f24e154b2916d9f445096cae
graua-git <graua@oregonstate.edu>
2023-08-20 19:41:57 -0700

    added comments for tasks endpoints

--- API/Endpoints/tasks.py
+++ API/Endpoints/tasks.py
@@ -7,14 +7,24 @@ tasks_bp = Blueprint('tasks', __name__)
 
 @tasks_bp.route('/read-all', methods=['GET'])
 def read_tasks():
+    """
+    Returns
+    task_id | name | trip | assignee | created_by | date_created | time_created | due_date | due_time
+    """
     sql = "SELECT * FROM Tasks"
     headers = ['task_id', 'name', 'trip', 'assignee', 'created_by', 'date_created', 'time_created', 'due_date', 'due_time']
     return read(sql, headers)
 
 @tasks_bp.route('/create', methods=['POST'])
 def create_task():
+    """
+    Creates task, requires info from json
+    """
     return create(request.get_json(), "Tasks")
 
 @tasks_bp.route('/update/<int:task_id>', methods=['PUT'])
 def update_task(task_id):
+    """
+    Updates task, requires info from json
+    """
     return update(request.get_json(), "Tasks", task_id)
