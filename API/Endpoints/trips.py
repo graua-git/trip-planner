2a1c2187b89d134d4af5ab80cd2d8ff1d6996aa2
graua-git <graua@oregonstate.edu>
2023-08-20 19:32:59 -0700

    added comments to trips endpoints

--- API/Endpoints/trips.py
+++ API/Endpoints/trips.py
@@ -7,12 +7,21 @@ trips_bp = Blueprint('trips', __name__)
 
 @trips_bp.route('/read-all', methods=['GET'])
 def read_trips():
+    """
+    Returns
+    trip_id | name | start_date | end_date
+    """
     sql = "SELECT * FROM Trips"
     headers = ['trip_id', 'name', 'start_date', 'end_date']
     return read(sql, headers)
 
 @trips_bp.route('/mytrips', methods=['GET'])
 def read_my_trips():
+    """
+    Returns
+    name | start_date | end_date | organizer
+    Where user_id = user_id from token
+    """
     # Validate token
     user_id = validate_token(request)
     if not isinstance(user_id, int):
@@ -33,8 +42,14 @@ def read_my_trips():
 
 @trips_bp.route('/create', methods=['POST'])
 def create_trip():
+    """
+    Creates trip, requires info from json
+    """
     return create(request.get_json(), "Trips")
 
 @trips_bp.route('/update/<int:trip_id>', methods=['PUT'])
 def update_trip(trip_id):
+    """
+    Updates trip, requires info from json
+    """
     return update(request.get_json(), "Trips", trip_id)
