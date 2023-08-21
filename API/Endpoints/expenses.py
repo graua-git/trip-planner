652f8ca0c998ef2180aa721d8ef967420b143126
graua-git <graua@oregonstate.edu>
2023-08-20 19:40:43 -0700

    added comments for expenses endpoints

--- API/Endpoints/expenses.py
+++ API/Endpoints/expenses.py
@@ -7,14 +7,24 @@ expenses_bp = Blueprint('expenses', __name__)
 
 @expenses_bp.route('/read-all', methods=['GET'])
 def read_expenses():
+    """
+    Returns
+    expense_id | name | trip | owed_to | owed_by | date_created | time_created | amount | settled
+    """
     sql = "SELECT * FROM Expenses"
     headers = ['expense_id', 'name', 'trip', 'owed_to', 'owed_by', 'date_created', 'time_created', 'amount', 'settled']
     return read(sql, headers)
 
 @expenses_bp.route('/create', methods=['POST'])
 def create_expense():
+    """
+    Creates expense, requires info from json
+    """
     return create(request.get_json(), "Expenses")
 
 @expenses_bp.route('/update/<int:expense_id>', methods=['PUT'])
 def update_expense(expense_id):
+    """
+    Updates expense, requires info from json
+    """
     return update(request.get_json(), "Expenses", expense_id)
