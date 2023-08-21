e27aa9ba75e8ae606535f321f759d6eba3c289a7
graua-git <graua@oregonstate.edu>
2023-08-20 19:43:43 -0700

    added comments for events endpoints

--- API/Endpoints/events.py
+++ API/Endpoints/events.py
@@ -7,14 +7,24 @@ events_bp = Blueprint('events', __name__)
 
 @events_bp.route('/events', methods=['GET'])
 def read_events():
+    """
+    Returns
+    event_id | name | trip | created_by | from_date | from_time | to_date | to_time
+    """
     sql = "SELECT * FROM Events"
     headers = ['event_id', 'name', 'trip', 'created_by', 'from_date', 'from_time', 'to_date', 'to_time']
     return read(sql, headers)
 
 @events_bp.route('/events', methods=['POST'])
 def create_event():
+    """
+    Creates event, requires info from json
+    """
     return create(request.get_json(), "Events")
 
 @events_bp.route('/events/<int:event_id>', methods=['PUT'])
 def update_event(event_id):
+    """
+    Updates event, requires info from json
+    """
     return update(request.get_json(), "Events", event_id)
